f4ebea4e6ab5bd29d4e1f5a2f34fdbe9aaaf17a5
paula-moraes <paula.moraes@kabum.com.br>
2025-11-07 17:28:03 -0300

    remove uso do gumga_token

--- run.py
+++ run.py
@@ -9,18 +9,16 @@ load_dotenv()
 
 CALLBACK_URL = os.getenv('CALLBACK_URL')
 API_KEY = os.getenv('API_KEY')
-GUMGA_TOKEN = os.getenv('GUMGA_TOKEN')
 
-if not all([CALLBACK_URL, API_KEY, GUMGA_TOKEN]):
-    print('Erro: Variáveis de ambiente CALLBACK_URL, API_KEY ou GUMGA_TOKEN não foram definidas')
+if not all([CALLBACK_URL, API_KEY]):
+    print('Erro: Variáveis de ambiente CALLBACK_URL ou API_KEY não foram definidas')
     sys.exit(1)
 
 
 def main():
     headers = {
         'apiKey': API_KEY,
-        'Content-Type': 'application/json',
-        'gumgaToken': GUMGA_TOKEN
+        'Content-Type': 'application/json'
     }
 
     payload = json.dumps({
